15b256f77c98c266c3785a088f0870602cc16a38
Roman <r.zharuk@ukr.net>
2022-01-02 00:48:40 +0200

    test1

--- Counter.cpp
+++ Counter.cpp
@@ -10,6 +10,26 @@ class Counter
 
 public:
 
+	int Set_min(unsigned int value_min)
+	{
+		this->value_min = value_min;
+	}
+
+	int Set_max(unsigned int value_max)
+	{
+		this->value_max = value_max;
+	}
+
+	int Set_cur_count(unsigned int cur_count)
+	{
+		this->cur_count = cur_count;
+	}
+
+	int Set_step(unsigned int step)
+	{
+		this->step = step;
+	}
+
 	Counter()
 	{
 		value_min = 0;
@@ -31,6 +51,11 @@ public:
 		return cur_count += step;
 	}
 
+	void Print()
+	{
+		cout << "value_min = " << value_min << "\nvalue_max = " << value_max <<
+			"\nstep = " << step << "\ncur_count = " << cur_count << "\n\n";
+	}
 
 };
 
@@ -39,6 +64,9 @@ int main()
 	setlocale(0, "");
 
 	Counter water;
-	water.
+	water.Print();
+	Counter(0, 999, 5, 100);
+	water.Print();
+	
 }
 
